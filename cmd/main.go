32527fca84a5c7dc0042aae80b49ae63a4812e0e
olegdayo <ol-sidorenkov@mail.ru>
2025-05-22 22:04:38 +0300

    more stuff

--- cmd/main.go
+++ cmd/main.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"context"
-	"fmt"
 	"log"
 	"os"
 	"os/signal"
@@ -16,7 +15,6 @@ func main() {
 	stopCtx, cancel := signal.NotifyContext(context.Background(), syscall.SIGTERM, os.Interrupt)
 
 	configPath := os.Args[1]
-	fmt.Println(configPath)
 	conf, err := config.Parse(configPath)
 	if err != nil {
 		log.Fatalf("Error during config parse: %+v", err)
